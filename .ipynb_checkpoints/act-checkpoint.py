42152f79da752ae5d5e59948e04ae266956c3a04
bekhzod-olimov <bekhzod.olimov@gmail.com>
2024-05-09 16:04:53 +0900

    del dirs

--- .ipynb_checkpoints/act-checkpoint.py
+++
@@ -1,7 +0,0 @@
-import torch
-from torch.nn import *
-
-class WiBReLU(Module): 
-    def __init__(self): super().__init__() 
-          
-    def forward(self, inp): return functional.relu(inp) - torch.mean(inp)
--- .ipynb_checkpoints/transformations-checkpoint.py
+++
@@ -1,7 +0,0 @@
-# Import library
-from torchvision import transforms as T
-
-# Get transformations based on the input image dimensions
-def get_tfs(im_size = (32, 32), imagenet_normalization = True, gray = None):
-    mean, std = [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
-    return T.Compose([T.Resize((im_size)), T.Grayscale(num_output_channels = 3), T.ToTensor(), T.Normalize(mean = mean, std = std)]) if gray else T.Compose([T.Resize((im_size)), T.ToTensor(), T.Normalize(mean = mean, std = std)])
